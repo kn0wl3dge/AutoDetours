53b747fe0dc9d004423187444c7bdf21dbcbd25d
Baptistin Boilot <baptistin.boilot@epita.fr>
2021-02-11 21:51:28 +0100

    Add malware database / API

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from malwaredb.views import MalwareViewSet
+
+router = routers.DefaultRouter()
+router.register(r'malware', MalwareViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
