6e65da01f3bcee9c3286c8a34a9ab319beff8bff
Baptistin Boilot <baptistin.boilot@epita.fr>
2021-02-12 15:56:12 +0100

    Add workers API

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_fsm',
     'malwaredb',
+    'workers'
 ]
 
 MIDDLEWARE = [
