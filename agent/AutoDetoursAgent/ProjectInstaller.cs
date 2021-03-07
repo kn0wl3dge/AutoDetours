755aab272fc6c19848e467bfdd797698db5c84fc
Kn0wl3dge <baptistin.boilot@epita.fr>
2021-03-07 17:06:46 +0100

    I forgot to commit, I am sorry :/
    * Refacto of Agent
    * Improve agent workflow (healthcheck, registration, randomness issues of uuid, cleanup)
    * Add a Qemu container pool system
    * Add Celery to the docker-compose

--- agent/AutoDetoursAgent/ProjectInstaller.cs
+++ agent/AutoDetoursAgent/ProjectInstaller.cs
@@ -15,5 +15,15 @@ namespace AutoDetoursAgent
         {
             InitializeComponent();
         }
+
+        private void serviceProcessInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+        }
+
+        private void serviceInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+        }
     }
 }
