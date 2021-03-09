545e515cafbf3dddf7515a4a6283b9d816bb23ec
ssg <ssg@local>
2021-03-10 00:38:46 +0100

    I don't what I've done, sorry...

--- backend/workers/tasks.py
+++ backend/workers/tasks.py
@@ -8,22 +8,23 @@ from workers.models import Worker, WorkerState
 @shared_task
 def worker_delete(ip):
     client = docker.DockerClient(base_url='unix://var/run/docker.sock')
-
+    print("Looking up container using ip %s for deletion" % ip)
     for c in client.containers.list():
-        print(c.attrs['NetworkSettings']['IPAddress'])
-        if c.attrs['NetworkSettings']['IPAddress'] == ip:
-            print("Killing container %s" % c.name)
-            c.kill()
-            break
+        for network, infos in c.attrs['NetworkSettings']['Networks'].items():
+            if infos['IPAddress'] == ip:
+                print("Killing container %s" % c.name)
+                c.kill()
+                break
 
 @shared_task
 def workers_timeout():
     for worker in Worker.objects.filter(state=WorkerState.TASKED):
-        if timezone.now() - worker.analysis_start_date * 2 > worker.malware.time:
+        delta = timezone.now() - worker.analysis_start_date
+        if delta.seconds > worker.malware.time * 3:
             print("Worker %s timed out !" % worker.id)
             worker.malware.end_analysis({"error": "Timed out.."})
             worker.malware.save()
-            worker_delete.s(worker.ip)
+            worker_delete.delay(worker.ip)
             worker.delete()
 
 @shared_task
