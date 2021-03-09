08d43c96d78afaae6333ca59f083f5f216973c80
Radion94200 <didier.zanello@epita.fr>
2021-03-09 16:56:57 +0100

    Create stats.vue - will disappear in the future

---
+++ frontend/pages/workers/stats.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-container fluid>
+    <h3>{{ stats.toto }}</h3>
+    <b-row class="mb-4">
+      <repartitioncharts :data> </repartitioncharts>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import repartitioncharts from '../../components/stats/repartitioncharts.vue'
+export default {
+  components: { repartitioncharts },
+  name: 'Stats',
+  async asyncData ({ app }) {
+    const data = await app.$axios.$get('/stats/')
+    return {
+      stats: data
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
