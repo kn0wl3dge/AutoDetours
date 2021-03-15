4de7767165ca915f3ffd344af82e5fdb3ac7c83f
Prolintos <thedarkillerof44@outlook.com>
2021-03-15 16:44:41 +0100

    pushing the work of tags implementation in views to victoria because my computer doesn't want me to work

--- backend/tags/tags.py
+++ backend/tags/tags.py
@@ -1,15 +1,15 @@
-from celery import Celery
-import rule
+from celery import shared_task
+import tags.rule as rule
+import tags.extract as extract
 
-BROKER_URL = 'redis://localhost:6379/0'
-
-celery_app = Celery('task', broker=BROKER_URL)
-
-rules = rule.get_db_rules('db_rules')
+rules = rule.get_db_rules('tags/db_rules')
 
 
-@celery_app.task
-def set_tags(api_calls):
+@shared_task
+def set_tags(json_string):
+    json_content = extract.convert_to_json(json_string)
+    api_calls = extract.extract_funcname_list(json_content)
+
     tags = []
     for rule in rules:
         for pattern in rule.patterns:
