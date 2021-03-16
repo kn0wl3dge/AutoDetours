ff5ccc8d4fc89e90ad29fa12004ec31b12ffc522
ssg <ssg@local>
2021-03-16 18:11:47 +0100

    Dataset frontend integration

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from malwaredb.views import MalwareViewSet, DatasetGenerationView, DatasetDownloadView
+from malwaredb.views import MalwareViewSet, DatasetGenerationView, \
+                            DatasetDownloadView, DatasetCheckView
 from workers.views import WorkerViewSet
 from stats.views import StatsView
 
@@ -30,5 +31,7 @@ urlpatterns = [
     path('api/stats/', StatsView.as_view()),
     path('api/dataset/generate/', DatasetGenerationView.as_view()),
     path('api/dataset/download/', DatasetDownloadView.as_view()),
-    path('api/dataset/download/<slug:pk>/', DatasetDownloadView.as_view())
+    path('api/dataset/download/<slug:pk>/', DatasetDownloadView.as_view()),
+    path('api/dataset/check/', DatasetCheckView.as_view()),
+    path('api/dataset/check/<slug:pk>/', DatasetCheckView.as_view())
 ]
