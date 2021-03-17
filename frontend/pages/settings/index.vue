ae2260b50592ec951e520b67708906b565fa843a
Prolintos <thedarkillerof44@outlook.com>
2021-03-17 14:02:06 +0100

    WIP tags front

---
+++ frontend/pages/settings/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-col>
+    <b-container fluid>
+      <h3>Settings</h3>
+      <b-row class="mb-4" />
+    </b-container>
+
+      <b-form-file
+        id="finput"
+        v-model="files"
+        :state="null"
+        size="md"
+        placeholder="Choose a rule file or drop it here..."
+        drop-placeholder="Drop files here..."
+        class="mb-2"
+        multiple
+      />
+  </b-col>
+</template>
+
+<script>
+import Tags from '../../components/settings/Tags'
+export default {
+  components: { Tags }
+}
+
+</script>
+
+<style scoped>
+</style>
