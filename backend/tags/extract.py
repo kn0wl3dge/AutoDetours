fffb007c6b349c6096ab10e21403ffbfe96399d5
Radion94200 <didier.zanello@epita.fr>
2021-03-19 17:10:23 +0100

    Remove if to catch all calls

--- backend/tags/extract.py
+++ backend/tags/extract.py
@@ -6,7 +6,6 @@ def extract_funcname_list(json_list):
     if not "error" in json_list:
         for api_call in json_list:
             call = api_call['funcName']
-            if call not in func_list:
-                func_list.append(call)
+            func_list.append(call)
         return func_list
     return ['error']
