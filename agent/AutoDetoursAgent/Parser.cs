98e69badb8f0790f12f5e26e4c9cb78fb9b04a27
Kn0wl3dge <baptistin.boilot@epita.fr>
2021-03-23 15:56:07 +0100

    Update parser codestyle

--- agent/AutoDetoursAgent/Parser.cs
+++ agent/AutoDetoursAgent/Parser.cs
@@ -61,11 +61,13 @@ namespace AutoDetoursAgent
         {
             return items.Count >= 5;
         }
+
         private static bool isThreadValid(String threadString)
         {
             long threadInt;
             return Int64.TryParse(threadString, out threadInt);
         }
+
         private static void DeleteSpaces(List<string> items)
         {
             for (int i = 0; i < items.Count;)
@@ -76,6 +78,7 @@ namespace AutoDetoursAgent
                     i++;
             }
         }
+
         private static bool isValidTrace(List<string> items)
         {
             if (items.Contains("Error") || items.Contains("error"))
@@ -86,8 +89,8 @@ namespace AutoDetoursAgent
 
             return isValidLengthForItems(items)
                 && isThreadValid(items[4]);
-
         }
+
         private static long ConvertToTimestamp(string timestamp8601)
         {
             DateTime value = DateTime.Parse(timestamp8601);
@@ -96,6 +99,7 @@ namespace AutoDetoursAgent
             TimeSpan elapsedTime = value - epoch;
             return (long)elapsedTime.TotalMilliseconds;
         }
+
         private static Tuple<string, long> FormatTimestamps(String timestamp)
         {
             string year = timestamp.Substring(0, 4);
@@ -113,6 +117,7 @@ namespace AutoDetoursAgent
 
             return Tuple.Create<string, long>(timestamp8601, epoch);
         }
+
         private static int isEntry(String functionCall)
         {
             if (functionCall.Length == 0)
@@ -121,6 +126,7 @@ namespace AutoDetoursAgent
                 return 1;
             return 0;
         }
+
         private static Tuple<string, List<string>> GetFuncEntryInfos(List<string> items)
         {
             // Remove the first char ('+')
@@ -138,10 +144,9 @@ namespace AutoDetoursAgent
                 funcParams.AddRange(new_params.GetRange(1, new_params.Count() - 1));
 
             }
-
-
             return Tuple.Create<string, List<string>>(funcName, funcParams);
         }
+
         private static Tuple<string, string> GetFuncOutputInfos(List<string> items)
         {
             string funcName = items[5].Substring(1).Split('(')[0];
@@ -155,6 +160,7 @@ namespace AutoDetoursAgent
             }
             return Tuple.Create<string, string>(funcName, funcOutput);
         }
+
         private static string ListToJson(List<string> jsonList)
         {
             string ret = "{\"results\": [";
@@ -171,6 +177,7 @@ namespace AutoDetoursAgent
 
             return (ret + "]}");
         }
+
         private static Log FindAssociatedLog(string funcName, List<Log> waitingOutput)
         {
             for (int i = (waitingOutput.Count() - 1); i >= 0; i--)
@@ -185,6 +192,7 @@ namespace AutoDetoursAgent
 
             return null;
         }
+
         private static void AddNotExitingLogs(List<string> jsonList, List<Log> waitingOutput)
         {
             for (int i = (waitingOutput.Count() - 1); i >= 0; i--)
@@ -193,6 +201,7 @@ namespace AutoDetoursAgent
                 jsonList.Add(waitingOutput[i].ToString());
             }
         }
+
         private static Log createLog(List<string> items, long start_time)
         {
             Log log = new Log();
@@ -210,6 +219,7 @@ namespace AutoDetoursAgent
 
             return log;
         }
+
         public static string ParseLogs(string input_filename)
         {
             List<string> jsonList = new List<string>();
