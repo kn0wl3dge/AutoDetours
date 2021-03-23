1ee72d2ed53ca9392380966e9f5bd9f077dbc552
Radion94200 <didier.zanello@epita.fr>
2021-03-23 15:07:55 +0100

    Back to back

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   extends: [
     '@nuxtjs',
     'plugin:nuxt/recommended',
-    'plugin:eslint-plugin-nuxt'
+    'eslint-plugin-nuxt'
   ],
   plugins: [],
   // add your custom rules here
