d134833f44008c50f76a7815e9f59d86fc3783d2
Kn0wl3dge <baptistin.boilot@epita.fr>
2021-04-19 23:07:53 +0200

    Clean frontend tags feature

--- backend/tags/tools/tool_yml.py
+++ backend/tags/tools/tool_yml.py
@@ -25,23 +25,3 @@ def write_rule(name, tag, functions):
         for func in functions:
             f.write("    - %s\n" % func)
         f.write("tag: %s" % tag)
-
-
-if __name__ == "__main__":
-    parser = ArgumentParser()
-
-    parser.add_argument("-n", "--name", help="Rule name", required=True)
-    parser.add_argument("-t", "--tag", help="Rule tag", required=True)
-    parser.add_argument(
-        "-f",
-        "--functions",
-        help="List of functions to add to rule. MSDN format.",
-        required=True,
-    )
-
-    args = parser.parse_args()
-    name = args.name
-    tag = args.tag
-    functions = extract_functions_from_file(args.functions)
-
-    write_rule(name, tag, functions)
