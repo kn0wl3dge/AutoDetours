cf137008fca185427f2bdbfcf2d51c4831372e3d
Kn0wl3dge <baptistin.boilot@epita.fr>
2021-04-19 22:41:27 +0200

    Clean tags feature

--- backend/workers/views.py
+++ backend/workers/views.py
@@ -3,20 +3,12 @@ from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.views import APIView
-
-from os import listdir
-from os.path import isfile, join, exists
 
 from workers.models import Worker, WorkerState
 from workers.serializers import WorkerSerializer
 from workers.tasks import worker_delete
 
 from tags.tags import set_tags
-from tags.rule import Rule
-from tags.rule import valid_filename
-
-RULES_PATHS = ["tags/db_rules", "/data/db_rules"]
 
 
 class WorkerViewSet(
@@ -71,61 +63,3 @@ class WorkerViewSet(
             else:
                 return Response({"error": "Can't find 'results' param"})
         return Response({"error": "Worker is in an incorrect state"})
-
-
-class RuleFormView(APIView):
-    def post(self, request):
-        rule = request.data["rule"]
-        if not valid_filename(rule):
-            return Response({"error": "Name format is incorrect"})
-        functions = request.data["functions"]
-        tag = request.data["tag"]
-        with open("/data/db_rules/" + rule.lower() + ".yml", "w") as f:
-            f.write("name: %s\n" % rule)
-            f.write("features:\n")
-            for func in functions:
-                f.write("   - %s\n" % func)
-            f.write("tag: %s" % tag)
-        return Response({"success": "Rule has been uploaded"})
-
-    def get(self, request):
-        rules_list = []
-        files = [
-            join(d, f)
-            for d in RULES_PATHS
-            if exists(d)
-            for f in listdir(d)
-            if isfile(join(d, f))
-        ]
-        for filename in files:
-            with open(filename, "r") as f:
-                content = f.readlines()
-                new_rule = Rule(name="", patterns=[], tag="")
-                is_pattern = False
-                for line in content:
-                    if "tag: " in line:
-                        is_pattern = False
-                        new_rule.tag = line.split(": ")[1].rstrip()
-                    if "name: " in line:
-                        new_rule.name = line.split(": ")[1].rstrip()
-                    elif "features:" in line:
-                        is_pattern = True
-                    elif is_pattern:
-                        new_rule.patterns.append(line.split("- ")[1].rstrip())
-                json_format = {
-                    "rule": new_rule.name,
-                    "functions": new_rule.patterns,
-                    "tag": new_rule.tag,
-                }
-                rules_list.append(json_format)
-        return Response(rules_list)
-
-    def delete(self, request):
-        name = request.query_params["rule"]
-        if valid_filename(name):
-            path = "/data/db_rules/" + name + ".yml"
-            if os.path.exists(path):
-                os.remove(path)
-            return Response({"success": "Rule as been deleted"})
-        else:
-            return Response({"error": "File is not valid"})
