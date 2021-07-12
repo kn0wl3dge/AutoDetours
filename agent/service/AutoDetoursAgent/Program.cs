9b6a442c0d32255bab0b8f459138064626431949
Kn0wl3dge <baptistin.boilot@epita.fr>
2021-07-12 21:06:21 +0200

    Setup project directory

---
+++ agent/service/AutoDetoursAgent/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDetoursAgent
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            AgentService agentService = new AgentService();
+            agentService.OnDebug();
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new AgentService()
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+        }
+    }
+
+}
