d620f1e6b41c5402dfc77f483a8c44615c726174
Kn0wl3dge <baptistin.boilot@epita.fr>
2021-08-01 17:43:06 +0200

    Fix #39 - Add an external snapshot to share a qcow2 between all workers using -snapshot mode

--- backend/workers/tasks.py
+++ backend/workers/tasks.py
@@ -8,6 +8,12 @@ from workers.models import Worker
 from jobs.models import JobState
 
 
+NETWORK = "autodetours_autodetours_lan"
+INPUT_DIR = os.environ.get("WIN7_IMAGES_DIR")
+NB_WORKERS = int(os.environ.get("NB_WIN7_WORKERS"))
+QEMU_IMAGE = "autodetours_qemu"
+
+
 @shared_task
 def worker_delete(ip):
     """Celery task used to remove a qemu container. This task
@@ -17,7 +23,9 @@ def worker_delete(ip):
     Args:
         ip (String): IP address of the container associated to the worker.
     """
+
     client = docker.DockerClient(base_url="unix://var/run/docker.sock")
+
     print("Looking up container using ip %s for deletion" % ip)
     for c in client.containers.list():
         for network, infos in c.attrs["NetworkSettings"]["Networks"].items():
@@ -33,6 +41,7 @@ def workers_timeout():
     This mean that the job is taking too much time or simply that something
     crashed.
     """
+
     for worker in Worker.objects.filter(job__state=JobState.RUNNING):
         delta = timezone.now() - worker.job.start_time
         limit = worker.job.job_time * 5
@@ -51,26 +60,31 @@ def workers_automation():
     of worker defined in the installation step.
     Its job is just to create container used to executes jobs.
     """
-    input_dir = os.environ.get("WIN7_IMAGES_DIR")
-    output_dir = "/image/"
-    network = "autodetours_autodetours_lan"
-    nb_workers = int(os.environ.get("NB_WIN7_WORKERS"))
-    workers = {}
-    for i in range(nb_workers):
-        workers["autodetours_workers_%i" % i] = "win7-%i.qcow2" % i
+
     client = docker.DockerClient(base_url="unix://var/run/docker.sock")
 
-    for worker, image in workers.items():
+    cmd = [
+        "-nographic",
+        "-m 1024",
+        "--enable-kvm",
+        "-snapshot",
+        '-incoming "exec: gzip -c -d /image/snapshot.gz"',
+        "-hda /image/win7.qcow2",
+    ]
+    cmd = " ".join(cmd)
+
+    for i in range(NB_WORKERS):
+        worker = f"autodetours_workers_{i}"
         try:
             client.containers.get(worker)
         except docker.errors.NotFound:
-            print("Runnning worker %s" % worker)
+            print(f"Runnning worker {worker}")
             client.containers.run(
-                "autodetours_qemu",
-                command=f"-nographic -m 1024 -loadvm agent -enable-kvm -hda {output_dir}{image}",
-                network=network,
+                QEMU_IMAGE,
+                command=cmd,
+                network=NETWORK,
                 devices=["/dev/kvm"],
-                volumes={input_dir: {"bind": output_dir, "mode": "rw"}},
+                volumes={INPUT_DIR: {"bind": "/image", "mode": "ro"}},
                 remove=True,
                 name=worker,
                 detach=True,
